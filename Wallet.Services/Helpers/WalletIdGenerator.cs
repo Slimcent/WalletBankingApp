45ed7317fff5143557f7d9b250c8b700109e44a5
Obinna <obinna.achara@outlook.com>
2022-01-22 13:07:14 +0100

    Changed a number in wallet generator

--- Wallet.Services/Helpers/WalletIdGenerator.cs
+++ Wallet.Services/Helpers/WalletIdGenerator.cs
@@ -7,7 +7,7 @@ namespace Wallet.Services.Helpers
     {
         public static string GenerateWalletId()
         {
-            string startWith = "22";
+            string startWith = "62";
             var miliseconds = string.Format("{0:000}", DateTime.Now.Millisecond);
             var year = DateTime.Now.ToString("yy");
             var day = RandomNumberGenerator.GetInt32(100, 999).ToString();
