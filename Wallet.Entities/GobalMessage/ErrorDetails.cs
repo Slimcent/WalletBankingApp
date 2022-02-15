3259384d34d31d7e6a5b88f2c981693dbb57778a
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-02-15 09:42:44 +0100

    Modified Global Exception Handler.

--- Wallet.Entities/GobalMessage/ErrorDetails.cs
+++ Wallet.Entities/GobalMessage/ErrorDetails.cs
@@ -1,10 +1,11 @@
 ﻿using System.Text.Json;
+using Wallet.Entities.Enumerators;
 
 namespace Wallet.Entities.GobalError
 {
     public class ErrorDetails
     {
-        public int StatusCode { get; set; }
+        public ResponseStatus Status { get; set; }
         public string Message { get; set; }
         public override string ToString() => JsonSerializer.Serialize(this);
 
--- WalletApi/Middlewares/GlobalExceptionMiddleware.cs
+++ WalletApi/Middlewares/GlobalExceptionMiddleware.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
 using System.Net;
+using Wallet.Entities.Enumerators;
 using Wallet.Entities.GobalError;
 using Wallet.Logger;
+using Wallet.Services.Exceptions;
 
 namespace WalletApi.Middlewares
 {
@@ -15,17 +19,43 @@ namespace WalletApi.Middlewares
             {
                 appError.Run(async context =>
                 {
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    //context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
                     context.Response.ContentType = "application/json";
+
                     var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
                     if (contextFeature != null)
                     {
+                        var status = ResponseStatus.FATAL_ERROR;
+
+                        switch (contextFeature.Error)
+                        {
+                            case InvalidDataException:
+                            case InvalidOperationException:
+                            case ArgumentException:
+                                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                                status = ResponseStatus.APP_ERROR;
+                                break;
+                            case NotFoundException:
+                                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                                status = ResponseStatus.NOT_FOUND;
+                                break;
+                            default:
+                                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                                break;
+                        }
+
                         logger.LogError($"Something went wrong: {contextFeature.Error}");
                         await context.Response.WriteAsync(new ErrorDetails()
                         {
-                            StatusCode = context.Response.StatusCode,
-                            Message = contextFeature.Error.InnerException.Message
+                            Status = status,
+                            Message = contextFeature.Error.Message
+                            //StatusCode = context.Response.StatusCode,
+                            //Message = contextFeature.Error.InnerException.Message
                         }.ToString());
+
+                        await context.Response.CompleteAsync();
                     }
                 });
             });
