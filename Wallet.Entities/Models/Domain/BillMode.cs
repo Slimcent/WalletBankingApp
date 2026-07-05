076ee4b7f48eb4a52a4e398ffef77edd566fa4f3
Obinna vincent Achara <obinna.achara@outlook.com>
2026-07-06 01:06:50 +0200

    added bill mode model.

---
+++ Wallet.Entities/Models/Domain/BillMode.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Wallet.Entities.Interfaces;
+
+namespace Wallet.Entities.Models.Domain
+{
+    public class BillMode : ITracker
+    {
+        [Column(TypeName = "varchar(256)")]
+        public string Id { get; set; }
+
+        [Unicode(false)]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public string BillId { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public virtual Bill Bill { get; set; }
+    }
+}
